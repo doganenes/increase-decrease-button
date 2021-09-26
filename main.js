0db19097976db0fe77430a3927b51db1c395bbc6
enesdogan <enesdoganlive@hotmail.com>
2021-09-26 11:19:11 +0300

    increase-decrease-button

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function arrtirma(){
+    var a = document.getElementById("text").value;
+a= Number(a);
+a+=1;
+document.getElementById("text").value=a;
+} 
+
+
+function azaltma(){
+    var a = document.getElementById('text').value;
+a = Number(a);
+a-=1;
+document.getElementById('text').value=a;
+}
